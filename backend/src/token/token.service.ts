26c11beebf6f5d57f319c806ec459fe0f0c4ee54
吴鑫 <wuxin.shawn@bytedance.com>
2023-12-05 19:03:58 +0800

    fix: create token

--- backend/src/token/token.service.ts
+++ backend/src/token/token.service.ts
@@ -15,16 +15,17 @@ export class TokenService {
     ) { }
 
     async genToken(username: string, password: string) {
+        const redis = RedisInstance.initRedis();
+        const failCount = await redis.get(username);
+        if (failCount != null && Number(failCount) >= MAX_TRY_NUM) {
+            throw new HttpException("A maximum of " + MAX_TRY_NUM + " in " + MAX_TRY_WITHIN_TIMES + "s please try later", HttpStatus.FORBIDDEN);
+        }
+
         const user = await this.userService.findOne(username);
         if (user?.password !== password) {
-            const redis = RedisInstance.initRedis();
-            const failCount = await redis.get(username);
             if (failCount == null) {
                 redis.setex(username, MAX_TRY_WITHIN_TIMES, 1)
                 throw new HttpException("login fail " + (MAX_TRY_NUM-1) + " times left in " + MAX_TRY_WITHIN_TIMES + "s", HttpStatus.UNAUTHORIZED);
-            }
-            if (Number(failCount) >= MAX_TRY_NUM) {
-                throw new HttpException("A maximum of " + MAX_TRY_NUM + " in " + MAX_TRY_WITHIN_TIMES + "s please try later", HttpStatus.FORBIDDEN);
             }
             redis.incr(username);
             throw new HttpException("login fail " + (MAX_TRY_NUM-Number(failCount)-1) + " times left in " + MAX_TRY_WITHIN_TIMES + "s", HttpStatus.UNAUTHORIZED);
